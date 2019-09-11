99b9794dd496ccb8e9ad6563a458fd255de50063
Louisa Nash <Louisa.Nash@softwire.com>
2019-09-11 14:46:48 +0100

    basic implementation works

--- bookishApp/bookishApp/Program.cs
+++ bookishApp/bookishApp/Program.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Dapper;
+using System.Configuration;
 
 namespace bookishApp
 {
@@ -16,9 +14,18 @@ namespace bookishApp
         static void Main(string[] args)
         {
             IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
-            string SqlString = "SELECT TOP 100 [CustomerID],[CustomerFirstName],[CustomerLastName],[IsActive] FROM [Customer]";
-            var ourCustomers = (List<Book>)db.Query<Book>(SqlString);
+            string SqlString = "SELECT TOP (1000) [ID], [AuthorName] FROM [bookish].[dbo].[Authors]";
+            var ourCustomers = (List<Author>)db.Query<Author>(SqlString);
 
+            foreach (var Customer in ourCustomers)
+            {
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine("\nID: " + Customer.ID.ToString());
+                Console.WriteLine("Name: " + Customer.AuthorName);
+                Console.WriteLine(new string('*', 20));
+            }
+
+            Console.ReadLine();
         }
     }
 }
